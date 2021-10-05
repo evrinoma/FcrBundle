924e599388edf05f3acedf24f5eb3daa1339591d
Nikolay Nikolaev <evrinoma@gmail.com>
2021-10-05 10:14:05 +0300

    initial DI

---
+++ src/DependencyInjection/EvrinomaFcrExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Evrinoma\FcrBundle\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class EvrinomaFcrExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        // TODO: Implement load() method.
+    }
+}
---
+++ src/EvrinomaFcrBundle.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Evrinoma\FcrBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EvrinomaFcrBundle extends Bundle
+{
+//region SECTION: Fields
+    public const FCR_BUNDLE = 'fcr';
+//endregion Fields
+
+//region SECTION: Getters/Setters
+    public function getContainerExtension()
+    {
+        if (null === $this->extension) {
+            $this->extension = new EvrinomaFcrExtension();
+        }
+
+        return $this->extension;
+    }
+//endregion Getters/Setters
+
+
+}
