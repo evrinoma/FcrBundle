16b35c5ef7fe9e238a942c4420ec5ae506fce65c
Nikolay Nikolaev <evrinoma@gmail.com>
2021-10-06 19:13:17 +0300

    fix DI and compilerPass

--- src/EvrinomaFcrBundle.php
+++ src/EvrinomaFcrBundle.php
@@ -2,8 +2,10 @@
 
 namespace Evrinoma\FcrBundle;
 
-
+use Evrinoma\FcrBundle\DependencyInjection\Compiler\DecoratorPass;
+use Evrinoma\FcrBundle\DependencyInjection\Compiler\MapEntityPass;
 use Evrinoma\FcrBundle\DependencyInjection\EvrinomaFcrExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EvrinomaFcrBundle extends Bundle
@@ -11,7 +13,14 @@ class EvrinomaFcrBundle extends Bundle
 //region SECTION: Fields
     public const FCR_BUNDLE = 'fcr';
 //endregion Fields
-
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container
+            ->addCompilerPass(new MapEntityPass($this->getNamespace(), $this->getPath()))
+            ->addCompilerPass(new DecoratorPass())
+        ;
+    }
 //region SECTION: Getters/Setters
     public function getContainerExtension()
     {
