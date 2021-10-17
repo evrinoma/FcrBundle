330e940284a4717bbb436d31a6b156cf054ab4cb
Nikolay Nikolaev <evrinoma@gmail.com>
2021-10-17 14:03:56 +0300

    fix static methods visibility from protected to public

--- src/Tests/Functional/Controller/ApiControllerTest.php
+++ src/Tests/Functional/Controller/ApiControllerTest.php
@@ -36,12 +36,12 @@ class ApiControllerTest extends CaseTest implements ApiControllerTestInterface, 
     }
 
 
-    protected static function getDtoClass(): string
+    public static function getDtoClass(): string
     {
         return FcrApiDto::class;
     }
 
-    protected static function defaultData(): array
+    public static function defaultData(): array
     {
         return [
             "id"          => '88',
