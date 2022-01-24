016e53c307361c92a10c7d5701f6a90fe74eadeb
Nikolay Nikolaev <evrinoma@gmail.com>
2022-01-24 18:13:39 +0300

    move constrains to property folder

--- src/Constraint/Property/Description.php
+++ src/Constraint/Property/Description.php
@@ -1,26 +1,26 @@
 <?php
 
 
-namespace Evrinoma\FcrBundle\Constraint;
+namespace Evrinoma\FcrBundle\Constraint\Property;
 
 use Evrinoma\UtilsBundle\Constraint\Property\ConstraintInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotNull;
 
-final class Id implements ConstraintInterface
+final class Description implements ConstraintInterface
 {
 //region SECTION: Getters/Setters
     public function getConstraints(): array
     {
         return [
             new NotBlank(),
-            new NotNull(),
+            new NotNull()
         ];
     }
 
     public function getPropertyName(): string
     {
-        return 'id';
+        return 'description';
     }
 //endregion Getters/Setters
 }
