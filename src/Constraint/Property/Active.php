b0da45f029d20dc6af2197a04f58cd5073d5dbd2
Nikolay Nikolaev <evrinoma@gmail.com>
2022-01-26 00:27:02 +0300

    fix configuration constraints and add Active constrains

---
+++ src/Constraint/Property/Active.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Evrinoma\FcrBundle\Constraint\Property;
+
+
+use Evrinoma\UtilsBundle\Constraint\Property\ActiveTrait;
+use Evrinoma\UtilsBundle\Constraint\Property\ConstraintInterface;
+
+class Active implements ConstraintInterface
+{
+//region SECTION: Getters/Setters
+    use ActiveTrait;
+
+//endregion Getters/Setters
+}
